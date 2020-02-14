8d4562982f9c5c90faf40129d310323f8fa0c2fb
DavidKellyonGitHub <dave.kelly.purchase@gmail.com>
2020-02-14 16:48:32 -0500

    added servicetests

--- src/main/java/services/SneakerService.java
+++ src/main/java/services/SneakerService.java
@@ -1,4 +1,41 @@
 package services;
 
+import models.Sneaker;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class SneakerService {
+    private static int nextId = 1;  // (1)
+
+    private ArrayList<Sneaker> inventory = new ArrayList<>();  // (2)
+
+    // (1)
+    public static Sneaker create(String name, String brand, String sport, int quantity, float price) {
+
+        // (2)
+        Sneaker createdSneaker = new Sneaker(nextId++, name, brand, sport, quantity, price);
+
+        // (3)
+        inventory.add(createdSneaker);
+
+        // (4)
+        return createdSneaker;
+    }
+
+    //read
+    public Sneaker findSneaker(int id) {
+        // should take an int and return an object with that id, if exists
+    }
+
+    //read all
+    public Sneaker[] findAll() {
+        // should return a basic array copy of the ArrayList
+    }
+
+    //delete
+    public boolean delete(int id) {
+        // should remove the object with this id from the ArrayList if exits and return true.
+        // Otherwise return false
+    }
 }
--- src/test/java/services/SneakerServiceTest.java
+++ src/test/java/services/SneakerServiceTest.java
@@ -1,4 +1,41 @@
 package services;
+import models.Sneaker;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class SneakerServiceTest {
+    @Test
+    public void createTest(){
+
+        // (1)
+        String expectedName = "Stan Smith";
+        String expectedBrand = "Adidas";
+        String expectedSport = "Tennis";
+
+        int expectedQty = 10;
+        float expectedPrice = 80.00f;
+
+        // (2)
+        SneakerService sneakerService = new SneakerService();
+        Sneaker testSneaker = SneakerService.create(expectedName, expectedBrand,
+                expectedSport,  expectedQty, expectedPrice);
+
+        // (3)
+        int actualId = testSneaker.getId(testSneaker);
+        String actualName = testSneaker.getName(testSneaker);
+        String actualBrand = testSneaker.getBrand(testSneaker);
+        String actualSport = testSneaker.getSport(testSneaker);
+        int actualQty = testSneaker.getQty(testSneaker);
+        float actualPrice = testSneaker.getPrice(testSneaker);
+
+        // (4)
+        Assertions.assertEquals(Integer.class.getName(), new Integer(actualId).getClass().getName());
+        Assertions.assertEquals(expectedName, actualName);
+        Assertions.assertEquals(expectedBrand, actualBrand);
+        Assertions.assertEquals(expectedSport, actualSport);
+
+        Assertions.assertEquals(expectedQty, actualQty);
+        Assertions.assertEquals(expectedPrice, actualPrice);
+
+    }
 }
